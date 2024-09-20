b4d6c1c72e9f3c20f22789b2315ffc48d514b3e1
khalid karam <khalid.tasks@gmail.com>
2024-09-20 23:42:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Abstraction_Interfaces3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var player = new Player();
+            player.Draw();
+            player.Move(10, 20);
+
+        }
+    }
+
+    public interface IDrawable
+    {
+        void Draw();
+    }
+
+    public interface IMovable
+    {
+        void Move(int x, int y);
+    }
+
+    public class Player : IDrawable, IMovable
+    {
+        public void Draw() => Console.WriteLine("Drawing Player");
+        public void Move(int x, int y) => Console.WriteLine($"Player moved to position ({x}, {y})");
+    }
+
+}
